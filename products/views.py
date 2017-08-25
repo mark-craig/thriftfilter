fb7a1c3ed2419e49a451fb0ca4bc24e4593ae987
Mark <mark.craig65@gmail.com>
2017-08-24 18:43:20 -0700

    Implemented basic index page displaying products

--- products/views.py
+++ products/views.py
@@ -3,7 +3,16 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Product
+
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Products listed here")
+	most_discounted_products = Product.objects.order_by('-markdown')[:100]
+	template = loader.get_template('products/index.html')
+	context = {
+		'most_discounted' : most_discounted_products
+	}
+	return HttpResponse(template.render(context, request))
