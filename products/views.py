9a5b5469ff5f21fbb3440e5ef23a1d059a7ae01a
Mark <mark.craig65@gmail.com>
2017-08-29 05:54:25 -0700

    Made improvements to UI, especially search box

--- products/views.py
+++ products/views.py
@@ -11,20 +11,27 @@ from .models import Product
 
 # Create your views here.
 def listings(request, department=None):
+	men_dep, wom_dep, all_dep = False, False, False
 	# get department parameter
 	if department == 'men':
 		department_results = Product.objects.all().filter(department="MEN'S")
+		men_dep = True
 	elif department == 'women':
 		department_results = Product.objects.all().filter(department="WOMEN'S")
+		wom_dep = True
 	else:
 		department_results = Product.objects.all()
+		all_dep = True
 
 	# get sort by query parameter
 	sort_by = request.GET.get('sortby')
+	sort_msg = 'Sorting by largest discount'
 	if sort_by == "lowtohigh":
 		most_discounted = department_results.order_by('current_price')
+		sort_msg = 'Sorting by lowest price'
 	elif sort_by == "hightolow":
 		most_discounted = department_results.order_by('-current_price')
+		sort_msg = 'Sorting by highest price'
 	else:
 		most_discounted = department_results.order_by('-markdown')
 
@@ -43,6 +50,9 @@ def listings(request, department=None):
 	template = loader.get_template('products/index.html')
 	context = {
 		'products' : products,
-		'department' : department
+		'men_dep' : men_dep,
+		'wom_dep' : wom_dep,
+		'all_dep' : all_dep,
+		'sort_msg' : sort_msg
 	}
 	return HttpResponse(template.render(context, request))
