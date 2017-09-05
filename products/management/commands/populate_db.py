e25f6257d4acdb5852e10e4cf5587b3951d95db3
Mark Craig <mark.craig65@gmail.com>
2017-09-04 21:06:14 -0700

    Updated populate_db script to handle duplicate products

--- products/management/commands/populate_db.py
+++ products/management/commands/populate_db.py
@@ -9,13 +9,13 @@ class Command(BaseCommand):
     help = 'This command populates the database with products given the data from JSON files in a directory.'
 
     def _add_products_from_dir(self):
-    	TEMP_JSON = os.path.join(os.path.abspath(os.sep), 'TEMP_JSON_FILES')
-    	for file in glob.glob(os.path.join(TEMP_JSON, "*.json")):
-    		with open(file) as json_data:
-    			d = json.load(json_data)
-    			# assuming the dictionary has identical parameter names as model
-    			p = Product(**d)
-    			p.save()
+        TEMP_JSON = os.path.join(os.path.expanduser('~'), 'TEMP_JSON_FILES')
+        for file in glob.glob(os.path.join(TEMP_JSON, "*.json")):
+            with open(file) as json_data:
+                # assume the dictionary has identical parameter names as model
+                d = json.load(json_data)
+                prod, created = Product.objects.update_or_create(
+                        name=d['name'], brand=d['brand'], defaults=d)
 
     def handle(self, *args, **options):
         self._add_products_from_dir()
